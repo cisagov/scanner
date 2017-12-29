d030f2be7e12a9257471b5354d2f00c9b811e31b
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2017-12-29 10:30:26 -0500

    Added --meta.  This adds some extra information to the CSV that may be useful for debugging.  I'm not writing this information to the database since it is different depending on whether the scan is run locally or via Lambda.

--- scan.sh
+++ scan.sh
@@ -17,7 +17,7 @@ redis-cli -h orchestrator_redis_1 del gathering_complete
 # Run the https-scan scan
 echo "Running domain-scan scan"
 cd $SHARED_DIR/artifacts/
-/home/scanner/domain-scan/scan $SHARED_DIR/scanme.csv --scan=pshtt,trustymail,sslyze --lambda --debug --cache --workers=400
+/home/scanner/domain-scan/scan $SHARED_DIR/scanme.csv --scan=pshtt,trustymail,sslyze --lambda --debug --meta --cache --workers=400
 
 # Clean up files no longer needed
 rm -rf $SHARED_DIR/artifacts/cache
