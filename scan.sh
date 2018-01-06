d7aa28b9c196a0ef4fbb5cf88c20d71365b5930e
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-01-06 14:49:33 -0500

    domain-scan deletes the existing CSV results files between runs, so I have to do a little trickery to save the pshtt results before kicking off trustymail and sslyze

--- scan.sh
+++ scan.sh
@@ -18,7 +18,12 @@ redis-cli -h orchestrator_redis_1 del gathering_complete
 echo "Running domain-scan scan"
 cd $SHARED_DIR/artifacts/
 /home/scanner/domain-scan/scan $SHARED_DIR/scanme.csv --scan=pshtt --lambda --lambda-details --debug --meta --cache --workers=550
+# domain-scan removes all existing CSV result files before starting a
+# new scan, so we need to stash the pshtt results in a safe place
+mv $SHARED_DIR/artifacts/results/pshtt.csv $SHARED_DIR/artifacts/pshtt.csv
 /home/scanner/domain-scan/scan $SHARED_DIR/scanme.csv --scan=trustymail,sslyze --debug --meta --cache --workers=50
+# Now put the pshtt results back
+mv $SHARED_DIR/artifacts/pshtt.csv $SHARED_DIR/artifacts/results/pshtt.csv
 
 # Clean up files no longer needed
 rm -rf $SHARED_DIR/artifacts/cache
