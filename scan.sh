8c5a735545fc9304d965536b53cb82a4dbeb43cd
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-01-11 09:28:15 -0500

    A couple of changes necessary since the gathered domains are now saved to the artifacts directory

--- scan.sh
+++ scan.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 SHARED_DIR='/home/scanner/shared'
 
 echo "Creating artifacts folder..."
@@ -17,11 +18,11 @@ redis-cli -h orchestrator_redis_1 del gathering_complete
 # Run the https-scan scan
 echo "Running domain-scan scan"
 cd $SHARED_DIR/artifacts/
-/home/scanner/domain-scan/scan $SHARED_DIR/scanme.csv --scan=pshtt --lambda --lambda-details --debug --meta --cache --workers=550
+/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=pshtt --lambda --lambda-details --debug --meta --cache --workers=550
 # domain-scan removes all existing CSV result files before starting a
 # new scan, so we need to stash the pshtt results in a safe place
 mv $SHARED_DIR/artifacts/results/pshtt.csv $SHARED_DIR/artifacts/pshtt.csv
-/home/scanner/domain-scan/scan $SHARED_DIR/scanme.csv --scan=trustymail,sslyze --debug --meta --cache --workers=50
+/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=trustymail,sslyze --debug --meta --cache --workers=50
 # Now put the pshtt results back
 mv $SHARED_DIR/artifacts/pshtt.csv $SHARED_DIR/artifacts/results/pshtt.csv
 
