ac3b44387f63273ddd949f4cc02330268fe17db8
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-01-16 22:03:51 -0500

    Put the pshtt scan back in

--- scan.sh
+++ scan.sh
@@ -18,13 +18,13 @@ redis-cli -h orchestrator_redis_1 del gathering_complete
 # Run the https-scan scan
 echo "Running domain-scan scan"
 cd $SHARED_DIR/artifacts/
-# /home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=pshtt --lambda --lambda-details --debug --meta --cache --workers=550
+/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=pshtt --lambda --lambda-details --debug --meta --cache --workers=550
 # domain-scan removes all existing CSV result files before starting a
 # new scan, so we need to stash the pshtt results in a safe place
-# mv $SHARED_DIR/artifacts/results/pshtt.csv $SHARED_DIR/artifacts/pshtt.csv
+mv $SHARED_DIR/artifacts/results/pshtt.csv $SHARED_DIR/artifacts/pshtt.csv
 /home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=trustymail,sslyze --debug --meta --cache --workers=50
 # Now put the pshtt results back
-# mv $SHARED_DIR/artifacts/pshtt.csv $SHARED_DIR/artifacts/results/pshtt.csv
+mv $SHARED_DIR/artifacts/pshtt.csv $SHARED_DIR/artifacts/results/pshtt.csv
 
 # Clean up files no longer needed
 rm -rf $SHARED_DIR/artifacts/cache
