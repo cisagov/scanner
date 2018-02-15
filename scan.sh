d05baf4028b13181ae52b00f5ab81551360af6af
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-02-15 13:32:20 -0500

    I removed the --lambda-details flag since it provides limited information and makes the run take much longer.

--- scan.sh
+++ scan.sh
@@ -18,7 +18,7 @@ redis-cli -h orchestrator_redis_1 del gathering_complete
 # Run the https-scan scan
 echo "Running domain-scan scan"
 cd $SHARED_DIR/artifacts/
-/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=pshtt --lambda --lambda-details --debug --meta --cache --workers=550
+/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=pshtt --lambda --debug --meta --cache --workers=550
 # domain-scan removes all existing CSV result files before starting a
 # new scan, so we need to stash the pshtt results in a safe place
 mv $SHARED_DIR/artifacts/results/pshtt.csv $SHARED_DIR/artifacts/pshtt.csv
