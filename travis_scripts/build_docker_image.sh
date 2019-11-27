544c0acf6943f7588f37383bb940226e8493ba30
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-11-27 11:02:15 -0500

    Quote some bash variables to make shellcheck happy

--- travis_scripts/build_docker_image.sh
+++ travis_scripts/build_docker_image.sh
@@ -3,4 +3,4 @@
 # semver uses a plus character for the build number (if present).
 # This is invalid for a Docker tag, so we replace it with a minus.
 version=$(./bump_version.sh show|sed "s/+/-/")
-docker build -t ${IMAGE_NAME}:$version .
+docker build -t "$IMAGE_NAME":"$version" .
--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -4,4 +4,4 @@ echo "$DOCKER_PW" | docker login -u "$DOCKER_USER" --password-stdin
 # semver uses a plus character for the build number (if present).
 # This is invalid for a Docker tag, so we replace it with a minus.
 version=$(./bump_version.sh show|sed "s/+/-/")
-docker push "$IMAGE_NAME":$version
+docker push "$IMAGE_NAME":"$version"
