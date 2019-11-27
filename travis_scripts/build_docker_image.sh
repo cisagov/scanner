bbfe48c2efaa21286b8deefe009ee8ca9bf624a3
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-11-27 12:14:04 -0500

    Add the standard "set -o" lines to the TravisCI bash scripts

--- travis_scripts/build_docker_image.sh
+++ travis_scripts/build_docker_image.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 # semver uses a plus character for the build number (if present).
 # This is invalid for a Docker tag, so we replace it with a minus.
 version=$(./bump_version.sh show|sed "s/+/-/")
--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 echo "$DOCKER_PW" | docker login -u "$DOCKER_USER" --password-stdin
 # semver uses a plus character for the build number (if present).
 # This is invalid for a Docker tag, so we replace it with a minus.
